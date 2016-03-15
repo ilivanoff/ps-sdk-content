cbc3dd71766702dfc71c39d8b9d307f5aca728f7
ilivanoff <azazello85@mail.ru>
2016-03-15 15:54:06 +0300

    init.sh

--- conf/init.sh
+++ conf/init.sh
@@ -6,11 +6,16 @@ if [ ! -d "$GPATH/.git" ]; then
 fi
 
 cd $GPATH
-
 git pull
 git submodule update --init
 git submodule foreach git checkout master
 git submodule foreach git pull origin master
 
+git config core.filemode false
+cd $GPATH/www/ps-content
+git config core.filemode false
+cd $GPATH/www/ps-includes
+git config core.filemode false
+
 sudo chown -R www-data:www-data $GPATH/www
-#sudo chmod -R 775 $GPATH/www
+sudo chmod -R 775 $GPATH/www
